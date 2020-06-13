87b8b5f98dab84e0aa734edfbb08e74db6afc2b0
Nicholas Henry <nicholas.henry@gmail.com>
2020-06-13 11:20:53 -0400

    Step 3: Create DB migrations and load data

--- mix.exs
+++ mix.exs
@@ -44,7 +44,9 @@ defmodule ElixirMonitoringProm.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:geo, "~> 3.1.0"},
+      {:geo_postgis, "~> 3.1.0"}
     ]
   end
 
